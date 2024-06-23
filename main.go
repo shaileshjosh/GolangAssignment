e497aa236b401263a9fd9e1ca2afe8ef1bd289ed
Shailesh Parkhi <shailesh.parkhi@joshsoftware.com>
2024-06-23 12:25:01 +0530

    Added struct example

--- main.go
+++ main.go
@@ -3,17 +3,116 @@ package main
 //packages are imported using import
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io" // io/ioutil moved to io
 	"net/http"
 	"strings"
+	"sync"
 )
 
 const GameOverString string = "Game over!"
 const YouWinString string = "Congratulations,you won!"
 const WordConstant string = "racecar"
 
+// 1."dev" is flag 2.false is default value 3."dev mode" is helper text
+var development = flag.Bool("dev", false, "dev mode")
+
+type HangmanGame struct {
+	Entries     map[string]bool
+	Placeholder []string
+	Chances     int
+	Word        string
+}
+
+func play(h *HangmanGame, wg *sync.WaitGroup) {
+
+	// create placeholder slice matching to length of word
+	for i := 0; i < len(h.Word); i++ {
+		h.Placeholder = append(h.Placeholder, "_")
+	}
+
+	chances := 8
+
+	for {
+
+		// evaluate a loss! If user guesses a wrong letter or the wrong word, they lose a chance.
+		userInput := strings.Join(h.Placeholder, "")
+
+		if chances == 0 && userInput != h.Word {
+			defer wg.Done()
+			fmt.Println(GameOverString)
+			break
+		}
+		// evaluate a win!
+		if userInput == h.Word {
+			defer wg.Done()
+			fmt.Println(YouWinString)
+			break
+		}
+
+		//Console display
+		fmt.Println()
+		fmt.Println(h.Placeholder)               // render the placeholder
+		fmt.Printf("Chances left:%d\n", chances) // render the chances left
+
+		keys := []string{}
+		for key, _ := range h.Entries {
+			keys = append(keys, key)
+		}
+
+		fmt.Println("Guesses: ", keys) //show the words/letters guessed till now.
+		fmt.Printf("Guess the word or letter:")
+
+		// take the input
+		str := ""
+		fmt.Scanln(&str)
+
+		if len(str) > 1 { //check input is  word or single character
+			if str == h.Word {
+				fmt.Println(YouWinString)
+				break
+			} else {
+				h.Entries[str] = true
+				h.Chances -= 1
+				continue
+			}
+		}
+
+		//compare and update entries ,placeholder and chances
+
+		_, exist := h.Entries[str]
+
+		if exist {
+			// already exist entry in the guesses
+			continue
+		}
+
+		h.Entries[str] = true
+		found := false // flag to check character found
+
+		//Iterate over the string to check character exists
+		for i, v := range h.Word {
+			if str == string(v) {
+				found = true
+				h.Placeholder[i] = string(v)
+			}
+		}
+		//if not found,decrease the chances
+		if !found {
+			chances -= 1
+		}
+
+	}
+
+}
+
 func getWord() string {
+
+	if *development {
+		return WordConstant
+	}
+
 	res, err := http.Get("https://random-word-api.herokuapp.com/word?number=5")
 	if err != nil {
 		return WordConstant
@@ -38,87 +137,18 @@ func getWord() string {
 
 func main() {
 
-	word := getWord()
+	flag.Parse() // parse the flag before use
 
-	// lookup for entries made by the user.
-	entries := map[string]bool{}
-
-	// list of "_" corrosponding to the number of letters in the word. [ _ _ _ _ _ ]
-	placeholder := []string{}
-
-	//create placeholder slice matching to length of word
-	for i := 0; i < len(word); i++ {
-		placeholder = append(placeholder, "_")
+	hangmanGameStruct := HangmanGame{
+		Entries:     map[string]bool{},
+		Placeholder: []string{},
+		Chances:     8,
 	}
+	hangmanGameStruct.Word = getWord()
 
-	chances := 8
+	var waitGroup sync.WaitGroup
 
-	for {
-
-		// evaluate a loss! If user guesses a wrong letter or the wrong word, they lose a chance.
-		userInput := strings.Join(placeholder, "")
-
-		if chances == 0 && userInput != word {
-			fmt.Println(GameOverString)
-			break
-		}
-		// evaluate a win!
-		if userInput == word {
-			fmt.Println(YouWinString)
-			break
-		}
-
-		//Console display
-		fmt.Println()
-		fmt.Println(placeholder)                 // render the placeholder
-		fmt.Printf("Chances left:%d\n", chances) // render the chances left
-
-		keys := []string{}
-		for key, _ := range entries {
-			keys = append(keys, key)
-		}
-
-		fmt.Println("Guesses: ", keys) //show the words/letters guessed till now.
-		fmt.Printf("Guess the word or letter:")
-
-		// take the input
-		str := ""
-		fmt.Scanln(&str)
-
-		if len(str) > 1 { //check input is  word or single character
-			if str == word {
-				fmt.Println(YouWinString)
-				break
-			} else {
-				entries[str] = true
-				chances -= 1
-				continue
-			}
-		}
-
-		//compare and update entries ,placeholder and chances
-
-		_, exist := entries[str]
-
-		if exist {
-			// already exist entry in the guesses
-			continue
-		}
-
-		entries[str] = true
-		found := false // flag to check character found
-
-		//Iterate over the string to check character exists
-		for i, v := range word {
-			if str == string(v) {
-				found = true
-				placeholder[i] = string(v)
-			}
-		}
-		//if not found,decrease the chances
-		if !found {
-			chances -= 1
-		}
-
-	}
+	waitGroup.Add(1)
+	go play(&hangmanGameStruct, &waitGroup)
+	waitGroup.Wait()
 }
